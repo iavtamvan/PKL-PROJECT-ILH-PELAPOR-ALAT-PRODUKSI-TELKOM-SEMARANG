3b3c801e6757750bc32021fb444c2460db955ae0
Iav <ade.fajr.ariav@gmail.com>
2019-08-30 08:54:55 +0700

    add splash

--- app/src/main/java/com/ilh/alpro_telkom/SplashActivity.java
+++ app/src/main/java/com/ilh/alpro_telkom/SplashActivity.java
@@ -30,7 +30,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        getSupportActionBar().hide();
         displayFirebaseRegId();
         mRegistrationBroadcastReceiver = new BroadcastReceiver() {
             @Override
